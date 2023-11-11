140defb6b7d22979c796791afb31f6f15c880f50
Amin Ben Othmen <aminos.bo.12@gmail.com>
2023-11-11 15:45:01 +0000

    ex 6

---
+++ td1.js
@@ -0,0 +1,10 @@
+function isLeapYear(year) {
+    if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+leapYear(2004) // true
+leapYear(2002) // false
